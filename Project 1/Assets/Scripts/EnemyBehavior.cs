402a1a8f53b72d724c06329ff1531040a60894b7
dapgar <dominicapgar2100@gmail.com>
2022-10-07 12:23:44 -0400

    > Finsihed Project 1 Ckpt 3

--- Project 1/Assets/Scripts/EnemyBehavior.cs
+++ Project 1/Assets/Scripts/EnemyBehavior.cs
@@ -18,5 +18,15 @@ public class EnemyBehavior : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        RotateTowardsTarget();
+    }
+
+    private void RotateTowardsTarget()
+    {
+        var offset = 90f;
+        Vector2 direction = player.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
     }
 }
--- Project 1/Assets/Scripts/EnemySpawning.cs
+++ Project 1/Assets/Scripts/EnemySpawning.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemySpawning : MonoBehaviour
 {
     [SerializeField] GameObject enemyPF;
+    [SerializeField] Transform player;
 
     [SerializeField] int maxEnemies;
     [SerializeField] float enemySpeed;
@@ -14,11 +15,17 @@ public class EnemySpawning : MonoBehaviour
 
     int numOfEnemies;
 
+    private void Start()
+    {
+        player = GameObject.Find("Car").GetComponent<Transform>();    
+    }
+
     private void Update()
     {
         if (numOfEnemies < maxEnemies)
         {
-            enemies.Add(Instantiate(enemyPF, spawnPoints[Random.Range(0, 4)].transform.position, transform.rotation, transform));
+            enemies.Add(Instantiate(enemyPF, spawnPoints[Random.Range(0, 4)].transform.position, 
+                transform.rotation = , transform));
             numOfEnemies++;
         }
     }
